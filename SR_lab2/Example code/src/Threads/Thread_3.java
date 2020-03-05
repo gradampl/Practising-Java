137acdd41f8c42065c3e02be619911114719e824
gradampl <gr_adam@o2.pl>
2020-03-05 21:33:55 +0100

    Making a thread by implementing the Runnable interface - example

---
+++ SR_lab2/Example code/src/Threads/Thread_3.java
@@ -0,0 +1,36 @@
+package Threads;
+
+/*
+Program demonstruje utworzenie klasy z interfejsem Runnable
+w celu umozliwienia dziedziczenia i uruchomienie takiego obiektu
+jako watku
+*/
+
+class Foo{
+    private String name;
+
+    public Foo(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+}
+
+public class Thread_3 extends Foo implements Runnable{
+
+    public Thread_3(String name){
+        super(name);
+    }
+
+    public void run() {
+        System.out.println("Thread " + this.getName() + " is running.");
+    }
+
+    public static void main(String[] args) throws InterruptedException{
+        Thread_3 runnable = new Thread_3("runnable");
+        Thread thread = new Thread(runnable);
+        thread.start();
+    }
+}
