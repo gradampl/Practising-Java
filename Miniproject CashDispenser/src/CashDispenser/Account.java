65007ed29e72a1d7dcda7d2852de73aabb07faa3
gradampl <gr_adam@o2.pl>
2020-03-06 23:51:39 +0100

    First class - Account

---
+++ Miniproject CashDispenser/src/CashDispenser/Account.java
@@ -0,0 +1,33 @@
+package CashDispenser;
+
+import java.util.regex.Pattern;
+
+public class Account {
+
+    protected String cardNumber;
+    private double balance;
+    protected String pinCode;
+
+    Account(String id, float initialBalance, String startPin){
+        cardNumber=id;
+        balance=initialBalance;
+        pinCode=startPin;
+    }
+
+    double CheckBalance(){
+        return balance;
+    }
+
+    boolean canWithdraw(double amountToWithdraw){
+        if(amountToWithdraw<=balance){
+            return true;
+        }else
+            return false;
+    }
+
+    double SetBalance(double amountToWithdraw){
+        double newBalance = balance - amountToWithdraw;
+        balance = newBalance;
+        return balance;
+    }
+}
