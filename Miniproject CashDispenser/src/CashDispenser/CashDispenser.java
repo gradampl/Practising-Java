3b19c7a3accc6afb6837e6295d508b9c1e3bc52c
gradampl <gr_adam@o2.pl>
2020-03-06 23:52:05 +0100

    Next class - CashDispenser

---
+++ Miniproject CashDispenser/src/CashDispenser/CashDispenser.java
@@ -0,0 +1,100 @@
+package CashDispenser;
+
+import java.util.*;
+import java.util.regex.Pattern;
+
+public class CashDispenser {
+
+    private Pattern pattern = Pattern.compile("-?\\d+(\\.\\d+)?");
+
+    public boolean isNumeric(String strNum) {
+        if (strNum == null) {
+            return false;
+        }
+        return pattern.matcher(strNum).matches();
+    }
+
+    public String getcNumber() {
+        System.out.println("Podaj numer karty.");
+        Scanner in = new Scanner(System.in);
+        String cNumber = in.next();
+        return cNumber;
+    }
+
+    public String getPin() {
+        System.out.println("Podaj PIN.");
+        Scanner in = new Scanner(System.in);
+        String pin = in.next();
+        return pin;
+    }
+
+    public boolean isPinOk(Account account, String pin){
+        if(pin.equals(account.pinCode))
+            return true;
+        else return false;
+    }
+
+    public String getChoice() {
+
+        System.out.println("Jaką operację chcesz wykonać?");
+        System.out.println("Sprawdzić stan konta - wybierz 1.");
+        System.out.println("Podjąć gotówkę z konta - wybierz 2.");
+        System.out.println("Zakończyć - wybierz 3.");
+
+        Scanner in = new Scanner(System.in);
+        String choice = in.next();
+        return choice;
+    }
+
+    public boolean checkChoice(Account account, String choice) {
+        if (!isNumeric(choice) || choice.isEmpty()) {
+            return false;
+        } else return true;
+    }
+
+    public void Start(Account account, String choice) {
+        switch (choice) {
+            case "1":
+                System.out.println("Stan Twojego konta wynosi " + account.CheckBalance());
+                break;
+            case "2":
+                System.out.println("Jaką sumę chcesz podjąć?");
+                Scanner in = new Scanner(System.in);
+                String howMuch = in.next();
+                if(!isNumeric(howMuch)){
+                    System.out.println("Podaj kwotę wpisując liczbę.");
+                    break;
+                }
+                else{
+                    double sum = Double.parseDouble(howMuch);
+                    boolean notTooMuch = false;
+                    notTooMuch = account.canWithdraw(sum);
+
+                    if (notTooMuch && !(sum<= 0)) {
+                        System.out.println("Podjąłeś " + Double.parseDouble(howMuch)
+                                + ". Stan Twojego konta obecnie wynosi "
+                                + account.SetBalance(Double.parseDouble(howMuch)));
+                    }
+                    else if(sum<=0){
+                        System.out.println("Podaj kwotę większą od zera.");
+                    }
+                    else {
+                        System.out.println("Nie możesz podjąć aż tyle, gdyż Stan Twojego konta wynosi "
+                                + account.CheckBalance());
+                    }
+                    break;
+                }
+            case "3":
+                System.exit(0);
+                break;
+            default:
+                System.out.println("Nie ma takiej opcji.");
+                break;
+        }
+    }
+}
+
+
+
+
+
