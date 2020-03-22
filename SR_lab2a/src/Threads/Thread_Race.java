ff914eff6b0a1946bf00e4bd050d6e875027f69b
gradampl <gr_adam@o2.pl>
2020-03-22 20:04:29 +0100

    Update Thread_Race.java

--- SR_lab2a/src/Threads/Thread_Race.java
+++ SR_lab2a/src/Threads/Thread_Race.java
@@ -12,7 +12,7 @@ class Athlete extends Thread{
     public void run(){
         double total = 0;
         for(int i = 0; i < arrayReceived.size(); i++ ){
-            total += Math.tan(Math.tan(arrayReceived.get(i)/2));
+            total += Math.tan(arrayReceived.get(i));
         }
     }
 }
@@ -24,17 +24,17 @@ public class Thread_Race {
     ArrayList<Athlete> athletes = new ArrayList<>();
     ArrayList<Double> arr = new ArrayList<>();
     Random random = new Random();
-    long repetitions;
+    long repetitions = 100;
     long startTime = System.currentTimeMillis();
     long stopTime = System.currentTimeMillis();
-    public long runTime;
+    public double runTime;
     Score_Table table = new Score_Table();
 
     public void startRace(){
         for(int p = 3; p <= 6; p++) {
 
             arrSize = (long) Math.pow(10, p);
-            repetitions = arrSize*((long)Math.pow(10,(8-(p+2))));
+//            repetitions = (long)Math.pow(10,(8-p));
 
             arr.clear();
 
@@ -58,21 +58,28 @@ public class Thread_Race {
                 athletes.get(j).start();
             }
 
-            System.out.println("------------------ Tablica długości " + arrSize + " ----------------------");
+            System.out.println("------------------ Tablica długości " + arrSize + " --------------------");
             System.out.println("\n");
             System.out.println("------------- W wyścigu bierze udział " + threads + " zawodników. --------------");
             System.out.println("\n");
 
             int s = 0;
+            long rep = 0;
 
             try {
                 for (int a = 0; a < threads; a++) {
                     startTime = System.currentTimeMillis();
-                    for(int r = 1; r <= repetitions; r++){
-                        athletes.get(a).join();
-                    }
-                    stopTime = System.currentTimeMillis();
-                    runTime = stopTime - startTime;
+                    stopTime = startTime;
+                    do{
+                        for(int r = 1; r <= repetitions; r++){
+                            athletes.get(a).join();
+                        }
+                        rep++;
+                        stopTime = System.currentTimeMillis();
+                    }while(stopTime-startTime<1000);
+
+//                    stopTime = System.currentTimeMillis();
+                    runTime = (double)(stopTime - startTime)/(rep * repetitions);
                     int athleteNumber = a+1;
                     table.recordScore(athleteNumber, runTime);
                     s++;
@@ -87,6 +94,10 @@ public class Thread_Race {
     }
 
     public static void main(String[] args) {
+        System.out.println("\n\nZadanie 2 - wyścig wątków\n\n");
+        System.out.println("Aby zmniejszyć błąd pomiaru czasów, operacje są powtarzane tak długo,");
+        System.out.println("aż mierzony czas będzie nie krótszy niż jedna sekunda." +
+                "\nCzas jednego przebiegu to iloraz czasu mierzonego przez liczbę powtórzeń.\n");
         Thread_Race racing = new Thread_Race();
         racing.startRace();
     }
@@ -95,14 +106,14 @@ public class Thread_Race {
 
 class Score_Table{
 
-    Hashtable<Integer, Long> scores = new Hashtable<>();
+    Hashtable<Integer, Double> scores = new Hashtable<>();
 
-    public void recordScore(int nthAthlete, long score){
+    public void recordScore(int nthAthlete, double score){
         scores.put(nthAthlete,score);
     }
 
     public void printScores(){
-        scores.forEach((nthAthlete, score) -> {System.out.println("Zawodnik " + nthAthlete + ", czas = " + score);});
+        scores.forEach((nthAthlete, score) -> {System.out.format("Zawodnik " + nthAthlete + ", czas (w milisekundach) = %.2e%n", score);});
     }
 
     public void clearTable(){
